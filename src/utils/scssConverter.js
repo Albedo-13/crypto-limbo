ef4110535ca19561eea8b6b530cfba3700bbf766
PavelProkopenya <pavel12minsk@mail.ru>
2023-08-11 15:13:05 +0300

    refactor: delete useless comments

--- src/utils/scssConverter.js
+++ src/utils/scssConverter.js
@@ -1,24 +1,16 @@
 export const convertScssToObject = (string) => {
-  let properties = string
+  // INPUT EXAMPLE:
+  // :export {\n error: #f74e2c;\n success: #06b470;\n extra: #ffba3a;}
+  const props = string
     .replaceAll("\n", "")
     .split(/[;:{} ]+/)
-    .slice(2, -1);  console.log("properties:", properties);
+    .slice(2, -1);
 
-  const parsed = JSON.stringify(properties)
+  const stringifiedProps = JSON.stringify(props)
     .replace("[", "{")
     .replace("]", "}")
     .replaceAll(",", ":")
     .replaceAll(/#\w{6}":/g, (match) => match.slice(0, -1) + ",");
-  console.log("parsed:", parsed)
 
-  const parsed2 = JSON.parse(parsed);
-  console.log(parsed2);
-  return parsed2;
-
-  // console.log(JSON.parse('{"success": "#06b470", "error": "#f74e2c"}'));
+  return JSON.parse(stringifiedProps);
 }
-
-// convertScssToObject("{ error: #f74e2c; success: #06b470; extra: #ffba3a;}");
-convertScssToObject(" :export {  error: #f74e2c;  success: #06b470;  extra: #ffba3a;}");
-
-// :export {  error: #f74e2c;  success: #06b470;  extra: #ffba3a;}
