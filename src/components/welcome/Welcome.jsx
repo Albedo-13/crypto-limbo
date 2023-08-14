4fa97b67a814476121dd803a14948bd383ffc9d8
PavelProkopenya <pavel12minsk@mail.ru>
2023-08-14 12:39:15 +0300

    feat: add notebook img

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@mui/material";
 import NorthEastIcon from "@mui/icons-material/NorthEast";
 
+import notebook from "../../assets/images/notebook.png";
 import "./welcome.scss";
 
 export const Welcome = () => {
@@ -8,29 +9,36 @@ export const Welcome = () => {
     <section className="welcome">
       <div className="container">
         <div className="welcome__wrapper">
-          <h1 className="welcome__name horizontal-separator__small-blue">Crypto Brains</h1>
-          <h2 className="welcome__title">Buy & Sell Crypto Easy With Crypto Limbo</h2>
-          <hr className="horizontal-separator__dotted" />
-          <p className="welcome__subtitle">
-            It is a long established fact that a reader will be distracted by the readable content of a page when
-            looking at its layout.
-          </p>
-          <Button
-            sx={{
-              width: 200,
-              marginRight: "30px",
-            }}
-            variant="contained"
-            endIcon={<NorthEastIcon size="small" />}>
-            Start Now
-          </Button>
-          <Button
-            sx={{
-              width: 200,
-            }}
-            variant="outlined">
-            Beginner's Guide
-          </Button>
+          <div>
+            <h1 className="welcome__name horizontal-separator__small-blue">Crypto Brains</h1>
+            <h2 className="welcome__title">Buy & Sell Crypto Easy With Crypto Limbo</h2>
+            <hr className="horizontal-separator__dotted" />
+            <p className="welcome__subtitle">
+              It is a long established fact that a reader will be distracted by the readable content of a page when
+              looking at its layout.
+            </p>
+            <Button
+              sx={{
+                width: 200,
+                marginRight: "30px",
+              }}
+              variant="contained"
+              endIcon={<NorthEastIcon size="small" />}
+            >
+              Start Now
+            </Button>
+            <Button
+              sx={{
+                width: 200,
+              }}
+              variant="outlined"
+            >
+              Beginner's Guide
+            </Button>
+          </div>
+          <div>
+            <img src={notebook} alt="notebook with statistics" />
+          </div>
         </div>
       </div>
     </section>
