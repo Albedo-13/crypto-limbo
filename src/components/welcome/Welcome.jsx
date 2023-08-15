76864e74d94cf9a30926e923b6cb1cfc8d212e1e
PavelProkopenya <pavel12minsk@mail.ru>
2023-08-15 10:30:07 +0300

    feat: partial implementation of welcome section

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -9,35 +9,38 @@ export const Welcome = () => {
     <section className="welcome">
       <div className="container">
         <div className="welcome__wrapper">
-          <div>
+          <div className="welcome__wrapper-left">
             <h1 className="welcome__name horizontal-separator__small-blue">Crypto Brains</h1>
             <h2 className="welcome__title">Buy & Sell Crypto Easy With Crypto Limbo</h2>
             <hr className="horizontal-separator__dotted" />
             <p className="welcome__subtitle">
-              It is a long established fact that a reader will be distracted by the readable content of a page when
+              It is a long established fact that a reader will be distracted by the readable content of a page when{" "}
+              <br />
               looking at its layout.
             </p>
-            <Button
-              sx={{
-                width: 200,
-                marginRight: "30px",
-              }}
-              variant="contained"
-              endIcon={<NorthEastIcon size="small" />}
-            >
-              Start Now
-            </Button>
-            <Button
-              sx={{
-                width: 200,
-              }}
-              variant="outlined"
-            >
-              Beginner's Guide
-            </Button>
+            <div className="welcome__buttons">
+              <Button
+                sx={{
+                  width: 200,
+                  marginRight: "30px",
+                }}
+                variant="contained"
+                endIcon={<NorthEastIcon size="small" />}
+              >
+                Start Now
+              </Button>
+              <Button
+                sx={{
+                  width: 200,
+                }}
+                variant="outlined"
+              >
+                Beginner's Guide
+              </Button>
+            </div>
           </div>
-          <div>
-            <img src={notebook} alt="notebook with statistics" />
+          <div className="welcome__wrapper-right">
+            <img className="welcome__img undraggable" src={notebook} alt="notebook with statistics" />
           </div>
         </div>
       </div>
