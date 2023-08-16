20728a98109612a0b8e30118a5d08d29bd2d8fd4
PavelProkopenya <pavel12minsk@mail.ru>
2023-08-16 10:46:32 +0300

    feat: start marketTrends section

--- src/components/marketTrends/MarketTrends.jsx
+++ src/components/marketTrends/MarketTrends.jsx
@@ -1,7 +1,12 @@
+import "./marketTrends.scss";
+
 export const MarketTrends = () => {
   return (
-    <>
-      <p>MarketTrends module</p>
-    </>
+    <section className="market-trends">
+      <div className="container">
+        <h2 className="market-trends__title">Market Trends</h2>
+        <div className="market-trends__filters">312</div>
+      </div>
+    </section>
   );
-}
+};
--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 import { Header } from "../header/Header";
 import { Welcome } from "../welcome/Welcome";
+import { MarketTrends } from "../marketTrends/MarketTrends";
 
 export const MainPage = () => {
   return (
     <>
       <Header />
       <Welcome />
+      <MarketTrends />
       <Link to="/second">to second page</Link>
       {/* //! remove bottom block on production */}
       <div style={{minHeight: 250, backgroundColor: "gray"}} />
