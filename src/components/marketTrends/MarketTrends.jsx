c6c9e3411234219ae0f780244bd6fd734069fb67
PavelProkopenya <pavel12minsk@mail.ru>
2023-08-18 10:10:23 +0300

    feat: add chartjs dummy

--- src/components/marketTrends/MarketTrends.jsx
+++ src/components/marketTrends/MarketTrends.jsx
@@ -8,8 +8,59 @@ import TrendingDownIcon from "@mui/icons-material/TrendingDown";
 
 import "./marketTrends.scss";
 
-import testGraph from "../../assets/temp/test-layout-graph.png";
 import { formatDigit, formatPercentage } from "../../utils/utils";
+import { Link } from "react-router-dom";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+// TODO: graph js
+// TODO: section header separator & button variant contained
+// FIND INFO: MUI Button toggle active class
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
+const labels = ["January", "February"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => Math.random(-1000, 1000) * 1000),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      pointRadius: 10,
+      pointHoverRadius: 15,
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => Math.random(-1000, 1000) * 1000),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+      pointRadius: 10,
+      pointHoverRadius: 15,
+    },
+  ],
+};
 
 export const MarketTrends = () => {
   const coins = useSelector((state) => state.currencies.data.slice(0, 6));
@@ -30,7 +81,7 @@ export const MarketTrends = () => {
       });
 
       return (
-        <div key={currency.id} className="market-trends-item">
+        <Link to="#" key={currency.id} className="market-trends-item">
           <div className="market-trends-item__image">
             <img src={currency.image} alt={currency.name} />
           </div>
@@ -43,9 +94,9 @@ export const MarketTrends = () => {
           </div>
           <div className="market-trends-item__current-price">{formatDigit(currency.current_price)}</div>
           <div className="market-trends-item__graph">
-            <img src={testGraph} alt="test to delete" />
+            <Line options={options} data={data} />
           </div>
-        </div>
+        </Link>
       );
     });
   };
@@ -79,7 +130,7 @@ export const MarketTrends = () => {
             </Button>
           </div>
         </div>
-        <div className="market-trends-items">{marketItemsList}</div>
+=        <div className="market-trends-items">{marketItemsList}</div>
       </div>
     </section>
   );
