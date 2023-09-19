1a19aee3ba9bc89cf4a549d3c3d71606d866c82c
PavelProkopenya <pavel12minsk@mail.ru>
2023-09-19 16:14:51 +0300

    styles: remove side effects of mui dark theme migration

--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -2,10 +2,10 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
-   "fontFamily": `"Source Sans 3", sans-serif`,
+    "fontFamily": `"Source Sans 3", sans-serif`,
   },
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
