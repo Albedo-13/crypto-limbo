6589b4cdb154338b7905c34bdd5ad54f6b8805f1
PavelProkopenya <pavel12minsk@mail.ru>
2023-10-20 15:29:01 +0300

    feat: partial implementation of header screen adaptation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,43 @@
-import Button from "@mui/material/Button";
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import classNames from "classnames";
+
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
 
 import "./header.scss";
 import logo from "../../assets/logo.webp";
-import classNames from "classnames";
 
 export const Header = () => {
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+  const renderHeaderLinks = () => {
+    return (
+      <>
+      
+      </>
+    );
+  }
+
+  const renderHeaderOverlay_maybedontneedimtootiredtothink = () => {
+    return (
+      <>
+      
+      </>
+    );
+  }
+
+  const handleBurgerMenuClickOpen = () => {
+    setIsBurgerMenuOpen(true);
+  }
+  
   return (
     <header className="header">
       <div className="container">
+        {/* <div className="header-">
+          
+        </div> */}
         <div className="header__wrapper">
           <div className="header-logo v-line_small">
             <Link to="/" className="header-logo__link">
@@ -49,6 +78,14 @@ export const Header = () => {
             <Button classes={{ root: "button-header-signup" }} variant="contained" component={Link} to="/signup">
               Sign Up
             </Button>
+          </div>
+          <div className="header-burger">
+            <Button className="mui-button-text_grey4" variant="text" component={Link} to="#">
+              Sign Up
+            </Button>
+            <IconButton onClick={handleBurgerMenuClickOpen} aria-label="expand" size="large">
+              <GridViewOutlinedIcon fontSize="inherit" />
+            </IconButton>
           </div>
         </div>
       </div>
