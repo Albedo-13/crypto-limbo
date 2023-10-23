ffb437e970ae7ad47730374a5d878c4c34992319
PavelProkopenya <pavel12minsk@mail.ru>
2023-10-23 11:05:27 +0300

    feat: header mobile adaptation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,86 +1,86 @@
-import { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import classNames from "classnames";
 
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import CloseIcon from "@mui/icons-material/Close";
 
 import "./header.scss";
 import logo from "../../assets/logo.webp";
 
 export const Header = () => {
   const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+  const location = useLocation();
 
-  const renderHeaderLinks = () => {
+  useEffect(() => {
+    handleBurgerMenuClickClose();
+  }, [location]);
+
+  const renderHeaderLinks = (isHiddenOnMobile) => {
+    const styles = classNames({ "header-nav-mobile-hidden": isHiddenOnMobile });
     return (
       <>
-      
+        <nav className={`header__links ${styles}`}>
+          <ul className="header__list">
+            <li>
+              <NavLink to="/market" end className={({ isActive }) => classNames("header__link", { active: isActive })}>
+                Market
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/road-map"
+                end
+                className={({ isActive }) => classNames("header__link", { active: isActive })}
+              >
+                Road Map
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/referral" className="header__link">
+                Referral
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={`header__buttons ${styles}`}>
+          <Button classes={{ root: "button-header-login" }} component={Link} to="/login">
+            Log In
+          </Button>
+          <Button classes={{ root: "button-header-signup" }} variant="contained" component={Link} to="/signup">
+            Sign Up
+          </Button>
+        </div>
       </>
     );
-  }
-
-  const renderHeaderOverlay_maybedontneedimtootiredtothink = () => {
-    return (
-      <>
-      
-      </>
-    );
-  }
+  };
 
   const handleBurgerMenuClickOpen = () => {
     setIsBurgerMenuOpen(true);
-  }
-  
+  };
+
+  const handleBurgerMenuClickClose = () => {
+    setIsBurgerMenuOpen(false);
+  };
+
+  const headerLinksMonitor = renderHeaderLinks(true);
+  const headerLinksMobile = renderHeaderLinks(false);
+  const mobileNavStyles = classNames("mobile-nav", { "mobile-nav-active": isBurgerMenuOpen });
+
   return (
     <header className="header">
       <div className="container">
-        {/* <div className="header-">
-          
-        </div> */}
         <div className="header__wrapper">
           <div className="header-logo v-line_small">
             <Link to="/" className="header-logo__link">
               <img className="header-logo__img undraggable" src={logo} alt="crypto limbo logo" />
             </Link>
           </div>
-          <nav className="header__links">
-            <ul className="header__list">
-              <li>
-                <NavLink
-                  to="/market"
-                  end
-                  className={({ isActive }) => classNames("header__link", { active: isActive })}
-                >
-                  Market
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/road-map"
-                  end
-                  className={({ isActive }) => classNames("header__link", { active: isActive })}
-                >
-                  Road Map
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/referral" className="header__link">
-                  Referral
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-          <div className="header__buttons">
-            <Button classes={{ root: "button-header-login" }} component={Link} to="/login">
-              Log In
-            </Button>
-            <Button classes={{ root: "button-header-signup" }} variant="contained" component={Link} to="/signup">
-              Sign Up
-            </Button>
-          </div>
+          {headerLinksMonitor}
           <div className="header-burger">
-            <Button className="mui-button-text_grey4" variant="text" component={Link} to="#">
+            <Button className="mui-button-text_grey4" variant="text" component={Link} to="/signup">
               Sign Up
             </Button>
             <IconButton onClick={handleBurgerMenuClickOpen} aria-label="expand" size="large">
@@ -89,6 +89,19 @@ export const Header = () => {
           </div>
         </div>
       </div>
+      <aside className={mobileNavStyles} onClick={handleBurgerMenuClickClose}>
+        <div className="mobile-nav__wrapper">
+          <IconButton
+            className="mobile-nav__close"
+            onClick={handleBurgerMenuClickClose}
+            aria-label="close"
+            size="large"
+          >
+            <CloseIcon />
+          </IconButton>
+          {headerLinksMobile}
+        </div>
+      </aside>
     </header>
   );
 };
